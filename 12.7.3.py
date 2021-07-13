aba6ce2e4960f883991ecaeae6201bd8a707bbbe
di-ex <85092768+di-ex@users.noreply.github.com>
2021-07-13 15:05:07 +0700

    Add files via upload
    Задание 12.7.3

---
+++ 12.7.3.py
@@ -0,0 +1,5 @@
+money = input("Введите сумму вклада: ")
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = [round(i * float(money), 2) for i in list(per_cent.values())]
+print("Накопленные средства за год:", deposit)
+print("Максимальная сумма, которую вы можете заработать —", max(deposit))
