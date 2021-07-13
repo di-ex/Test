665c674436f3e28ec9e365601a2997955b386094
di-ex <85092768+di-ex@users.noreply.github.com>
2021-07-13 15:06:24 +0700

    Delete 12.7.3.py

--- 12.7.3.py
+++
@@ -1,5 +0,0 @@
-money = input("Введите сумму вклада: ")
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-deposit = [round(i * float(money), 2) for i in list(per_cent.values())]
-print("Накопленные средства за год:", deposit)
-print("Максимальная сумма, которую вы можете заработать —", max(deposit))
