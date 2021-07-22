973ca3c31236c6c49c6fc77cc23f23ba9e19db17
di-ex <85092768+di-ex@users.noreply.github.com>
2021-07-22 19:33:52 +0700

    Основы Python

--- 12.7.3.py
+++ 12.7.3.py
@@ -1,5 +1,5 @@
-money = input("Введите сумму вклада: ")
+money = float(input("Введите сумму вклада: "))
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-deposit = [round(i * float(money), 2) for i in list(per_cent.values())]
+deposit = [round(money + (i * money) / 100, 2) for i in list(per_cent.values())]
 print("Накопленные средства за год:", deposit)
 print("Максимальная сумма, которую вы можете заработать —", max(deposit))
