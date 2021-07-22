b52a7852dcca6965b480fb5730bbe7c15e8070d1
di-ex <85092768+di-ex@users.noreply.github.com>
2021-07-22 18:58:25 +0700

    Основы Python

---
+++ 13.3.py
@@ -0,0 +1,7 @@
+import re
+pas = input('Введите пароль: ')
+while not pas or re.search(r'[\s]', pas):
+    print("Некорректный пароль!")
+    pas = input('Введите пароль: ')
+else:
+    print("Пароль успешно создан")
