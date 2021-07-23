5f65dd133a64b0e625e3dd6cc24aa6fdc9319366
di-ex <85092768+di-ex@users.noreply.github.com>
2021-07-23 18:52:14 +0700

    Update 13.8.19.py

--- 13.8.19.py
+++ 13.8.19.py
@@ -1,14 +1,14 @@
 n = int(input('Сколько билетов хотите купить: '))
 age = input('Введите возраст посетителей через пробел: ')
 age = list(map(int, age.split()))
-sum = 0
+summa = 0
 for i in age:
     if i < 18:
-        sum += 0
+        summa += 0
     if 18 <= i < 25:
-        sum += 990
+        summa += 990
     if i >= 25:
-        sum += 1390
+        summa += 1390
 if n > 3:
-    sum -= sum * 0.1
-print('Сумма к оплате:', sum)
+    summa -= summa * 0.1
+print('Сумма к оплате:', summa)
